0205bf88937ef8fa881b134c4a74bfc5d98e3951
myattasaurus <rcoins84@yahoo.com>
2023-03-04 16:23:46 -0500

    Rename main activity

--- BeerAdviser/app/src/main/java/com/mattsploration/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/mattsploration/beeradviser/FindBeerActivity.java
@@ -1,14 +1,13 @@
 package com.mattsploration.beeradviser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_find_beer);
     }
 }
