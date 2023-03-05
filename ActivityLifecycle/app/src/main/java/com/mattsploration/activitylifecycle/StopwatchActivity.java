d3c181384fcc872e7f6cb6911261d6f2574fd877
myattasaurus <rcoins84@yahoo.com>
2023-03-05 14:03:24 -0500

    Initial commit chapter 4

---
+++ ActivityLifecycle/app/src/main/java/com/mattsploration/activitylifecycle/StopwatchActivity.java
@@ -0,0 +1,13 @@
+package com.mattsploration.activitylifecycle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
