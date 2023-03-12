c4c488e4274491278e5a5869f67e26c77811c08e
myattasaurus <rcoins84@yahoo.com>
2023-03-11 19:24:44 -0500

    Styling Messenger initial commit

---
+++ StylingMessenger/app/src/main/java/com/mattsploration/stylingmessenger/MessengerActivity.java
@@ -0,0 +1,13 @@
+package com.mattsploration.stylingmessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MessengerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messenger);
+    }
+}
